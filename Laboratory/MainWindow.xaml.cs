19404584dd5f88dd780b44cb360afbba0a12fe91
Владислав Кузнецов <kuznetsov_vv@outlook.com>
2021-12-05 15:51:42 +1000

    Добавьте файлы проекта.

---
+++ Laboratory/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Laboratory.db;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laboratory
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // часть кода для импорта
+            var connection = DBInstance.Get();
+            //connection.Gender.Add(new Gender { Code = "м", Name = "мужской" });
+            //connection.Gender.Add(new Gender { Code = "ж", Name = "женский" });
+            //connection.SaveChanges();
+            string path = @"C:\Users\kuzne\OneDrive\Документы\Учеба\КЗ_РЧ20_21_Основная группа\Session 1\services.csv";
+            var rows = File.ReadAllLines(path);
+            var clients = connection.Client.ToList();
+            var services = connection.Service.ToList();
+            for (int i = 1; i < rows.Length; i++)
+            {
+                var cols = rows[i].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                connection.Service.Add(new Service
+                    {
+                    id = i,
+                    servicecode = int.Parse(cols[0]),
+                    servicename = cols[1],
+                    price = double.Parse(cols[2].Replace('.',',')),
+            });
+
+        }
+           // connection.SaveChanges();
+        }
+    }
+}
---
+++ Laboratory/db/DBInstance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory.db
+{
+    public  class DBInstance
+    { 
+    static LabEntities connection;
+    static object objectLock = new object();
+    public static LabEntities Get()
+    {
+        lock (objectLock)
+        {
+            if (connection == null)
+                connection = new LabEntities();
+            return connection;
+        }
+    }
+    }
+}
